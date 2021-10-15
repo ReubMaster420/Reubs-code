db0a991cba0b941f36ea9bbd0749e70145115969
Reuben Larsen <203626@gardenschool.edu.my>
2021-10-15 21:21:51 +0800

    done

--- BinaryTreeCode1/BinaryTreeExamQ.py
+++ BinaryTreeCode1/BinaryTreeExamQ.py
@@ -5,27 +5,49 @@ class Node:
         self.RightPointer = RightPointer
 
 BinaryTree = []
+
 for i in range(0,12):
-    BinaryTree.append(node(0))
+    BinaryTree.append(Node(0))
+
+global NextNode
+NextNode = 0
+
 def CreateTree(NodeData):
     global NextNode
-    NextNode = 0
-    BinaryTree[NextNode].DataValue = NodaData
+    BinaryTree[NextNode].DataValue = NodeData
     NextNode = NextNode + 1
 
 def AttachLeft(NodeData, ParentNode):
+    global NextNode
     BinaryTree[NextNode].DataValue = NodeData
     BinaryTree[ParentNode].LeftPointer = NextNode
     NextNode = NextNode + 1
 
 def AttachRight(NodeData, ParentNode):
+    global NextNode
     BinaryTree[NextNode].DataValue = NodeData
     BinaryTree[ParentNode].RightPointer = NextNode
     NextNode = NextNode + 1
     
-def printTreeLeft(start, traversal):
-    if start:
-        traversal +=(str(start.value) + "-")
-        traversal = self.preorder_print(start.LeftPointer, traversal)
-    return traversal
+def printTreeLeft(binaryTree):
+    while True:
+        if BinaryTree is None: return
+        else:
+            printTreeLeft(BinaryTree.LeftPointer)
+
     
+CreateTree("Red")
+AttachLeft("Blue", 0)
+AttachRight("Green", 0)
+AttachRight("Black", 2)
+AttachLeft("Brown", 2)
+AttachLeft("Peach", 3)
+AttachLeft("Yellow", 1)
+AttachRight("Purple", 1)
+AttachLeft("White", 6)
+AttachLeft("Pink", 7)
+AttachLeft("Grey", 9)
+AttachRight("Orange", 9)
+
+printTreeLeft(BinaryTree)
+
