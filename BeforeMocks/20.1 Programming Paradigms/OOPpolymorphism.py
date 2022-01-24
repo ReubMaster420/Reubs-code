3fb0e7c90b7ce1d9bde7550e74246de628113f86
Reuben Larsen <203626@gardenschool.edu.my>
2022-01-24 10:23:15 +0800

    done

---
+++ BeforeMocks/20.1 Programming Paradigms/OOPpolymorphism.py
@@ -0,0 +1,30 @@
+class shape:
+    def __init__(self):
+        self.__areaValue= 0
+        self.__perimeterValue = 0
+    def area(self):
+        print("Area ", self.__areaValue)
+    def perimeter(self):
+        print("Perimeter ", self.__perimeterValue)
+class rectangle(shape):
+    def __init__(self, length, breadth):
+        shape.__init__(self)
+        self.__length = length
+        self.__breadth = breadth
+    def area(self):
+        self.__areaValue= self.__length*self.__breadth
+        print("Area ", self.__areaValue)
+class circle(shape):
+    def __init__(self, radius):
+        shape.__init__(self)
+        self.__radius = radius
+    def area(self):
+        self.__areaValue= 3.142*self.__radius*self.__radius
+        print("Area ", self.__areaValue)
+myCircle = circle(20)
+myCircle.area()
+myrec = rectangle(10,20)
+myrec.area()
+
+
+    
